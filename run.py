6dcebd60f8548aacace039ce7501ba7c28cd66c7
JavierAFT <javierantoniofelixtorres@gmail.com>
2022-01-31 23:23:52 -0700

    Hemos añadido nuevas rutas y nuevos templates

--- run.py
+++ run.py
@@ -1,15 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 #creamos la ruta con sus funciones
 @app.route('/', methods=['GET'])
 def holamundo():
-    return 'Hola Mundo'
+    return render_template('/index.html')
 
+#MIS PROYECTOS
 @app.route('/mis-proyectos', methods=['GET'])
 def mis_proyectos():
-    return 'Mis proyectos'
+    return render_template('/mis-proyectos.html')
+
+#MI BLOG
+@app.route('/blog', methods = ['GET'])
+def blog():
+    return render_template('/blog.html')
+
+#CONTACTO
+@app.route('/contacto', methods = ['GET'])
+def contacto():
+    return render_template('/contacto.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)

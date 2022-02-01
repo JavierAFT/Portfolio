cd429f5f508a2bdc7a833753a1e02f4dfe268eca
JavierAFT <javierantoniofelixtorres@gmail.com>
2022-01-31 23:09:06 -0700

    Agregamos carpeta template y creamos archivos index y contacto

--- run.py
+++ run.py
@@ -3,11 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 #creamos la ruta con sus funciones
-@app.route('/')
+@app.route('/', methods=['GET'])
 def holamundo():
     return 'Hola Mundo'
 
-@app.route('/mis-proyectos')
+@app.route('/mis-proyectos', methods=['GET'])
 def mis_proyectos():
     return 'Mis proyectos'
 
